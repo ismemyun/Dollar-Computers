58bbe66f1dda617fa2d617f11980a0b04f8eef1d
ismemyun <nancy.4766@gmail.com>
2019-07-19 17:23:13 -0400

    Revert "GUI"
    This reverts commit 94841abd3d719b03ef500d9989a0912cbd696427.

---
+++ ismemyun/COMP123-S2019-Assignment 4-BMI Calculator App/COMP123-S2019-Assignment 4-BMI Calculator App/BMICalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Assignment_4_BMI_Calculator_App
+{
+    public partial class BMICalculator : Form
+    {
+        public BMICalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void ImperialradioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            HeightUnitLabel.Text = "Inch";
+            WeighUnitLabel.Text = "Pound";
+        }
+
+        private void MetricradioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            HeightUnitLabel.Text = "cm";
+            WeighUnitLabel.Text = "kg";
+        }
+    }
+}
